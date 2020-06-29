60dfd0d10a64d3f1f11ab204d9eb17ef61a7ec56
eslessons <66363651+eslessons@users.noreply.github.com>
2020-06-29 12:12:24 +0300

    03 Types Scope

--- 02 Language Basics/script.js
+++ 02 Language Basics/script.js
@@ -1,1 +1,174 @@
-//  Hello world :)
+// // var vaiable = 5;
+// // var variable = 'text';
+// var variable = true;
+// // var variable = false;
+
+// console.log(variable);
+
+// // var var1 = 'text';
+// // var var1 = undefined;
+// // var var1 = NaN;
+// // var var1 = [1, 2, 3, '4'];
+
+// // "use strict";
+// // var var1 = 5
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// var var1 = {
+//   name: 'Max',
+// };
+
+// console.log(var1['2']);
+// console.log(var1);
+// console.log(typeof var1);
+
+// var Var1 = 10;
+// console.log(typeof Var1);
+// console.log(Var1);
+
+// // function calc() {
+// //   console.log('Inside function!');
+// // }
+
+// var calc = function () {
+//   console.log('Inside function');
+// };
+// calc();
+
+// // var anotherFn = calc;
+// // anotherFn();
+
+// function calc2() {
+//   var stringVar = 'Inside function';
+//   return stringVar;
+// }
+
+// var returned = calc();
+// console.log(returned);
+
+// function calc3(number1, number2) {
+//   return number1 + number2;
+// }
+
+// var returned = calc(2, 8);
+// console.log(returned);
+
+// var calculator = calc;
+// console.log(calc(15, 2));
+
+// var condition = true;
+// var anotherCondition = false;
+
+// if (condition) {
+//   console.log('Executed!');
+// } else if (anotherCondition) {
+//   console.log('Still Executed!');
+// } else {
+//   console.log('Not Executed');
+// }
+
+// console.log(1 == true);
+// console.log(2 == true);
+
+// var luckyNumber = 7;
+// var luckyNumber = 8;
+
+// switch (luckyNumber) {
+//   case 1:
+//     console.log('Is 1');
+// // break;
+//   case 8:
+//     console.log('Is 8');
+//     break;
+//   default:
+//     console.log('Default');
+//     break;
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   for (var j = 0; j < 2; j++) {
+//     if (i == 2) {
+//       continue;
+//     }
+//     if (i == 4) {
+//       break;
+//     }
+//     console.log(i * j);
+//   }
+// }
+
+// var array = [1,2,3]
+
+// for (var i = 0; i < 3; i++) {
+//   console.log(i.length);
+//   console.log(array[i]);
+// }
+
+// var number = 5;
+
+// while (number < 7) {
+//   console.log(number);
+//   number++;
+// }
+
+// var condition = true;
+// var i = 2;
+
+// while (condition) {
+//   if (i == 3) {
+//     condition = false;
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// var condition = false;
+// do {
+//   console.log('Executed');
+// } while (condition)
+
+// var a = [1, 2];
+// var a = 'Join';
+// var a = true;
+// var b = ' us';
+// var b = true;
+// var a = 5;
+// var b = 10;
+// a += b;
+// console.log(a)
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log((a * b).toFixed(2));
+
+// var b = Infinity;
+// console.log(b);
+
+// console.log(1 == 1);
+// console.log( 1 >= 1);
+// console.log(1 === 1);
+// console.log(2 != 1);
+
+// console.log(NaN === NaN); false
+// console.log(0 == null); false
+// console.log(null == undefined); true
+// console.log(0 == undefined); false
+
+// if (1 == 1 && 2 == 2) {
+//   console.log('true');
+// } else {
+//   console.log(false);
+// }
+
+// console.log((1==1) || (2==3) || (4==5));
+
+// var a = 5;
+// var b = 5;
+
+// console.log(a == b ? 'Equal!' : 'Not equal!');
+
+// var a = 5;
+// var b = 6;
+// console.log(a + b * 2);
